8d0d61e0fa3c9f8db3ded21fc6380d7378204b0c
haesongkk <haesoooong@gmail.com>
2025-10-10 03:26:54 +0900

    오디오 출력 기능 추가 및 UI 보완

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # SE_Tetris_Team1
+
+8-bit-game-music-122259.mp3
+https://pixabay.com/ko/music/%EB%B9%84%EB%94%94%EC%98%A4-%EA%B2%8C%EC%9E%84-8-bit-game-music-122259/
+
+gameboy-pluck-41265.mp3
+https://pixabay.com/ko/sound-effects/gameboy-pluck-41265/
+
+Giants-Bold.ttf 외 2개
+https://www.giantsclub.com/html/?pcode=1007
+
---
+++ src/main/java/tetris/util/Sound.java
@@ -0,0 +1,79 @@
+package tetris.util;
+
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+import java.io.InputStream;
+
+// ONLY MP3
+public class Sound {
+    static int counter = 0;
+    
+    String origin = null; 
+    volatile boolean running = false;
+    volatile Thread thread = null;
+    
+    public Sound(String filePath) {
+        this.origin = filePath;
+    }
+
+    public synchronized void play(boolean loop) {
+        release();
+
+        this.running = true;
+        thread = new Thread(() -> {
+            while (this.running) {
+
+                // 1) 리소스 로드
+                InputStream in = Thread.currentThread()
+                    .getContextClassLoader()
+                    .getResourceAsStream(origin);
+                        
+                if (in == null) {
+                    System.err.println("리소스 로드 실패: " + origin);
+                    break;
+                }
+
+                // 2) player 객체 생성 및 재생
+                Player player = null;
+                try { 
+                    player = new Player(in); 
+                    while (this.running) 
+                        if(!player.play(1)) break;
+                } catch(JavaLayerException e) {
+                    System.out.println("player 에러");
+                    this.running = false;
+                } finally {
+                    if(player != null) {
+                        try { player.close(); } 
+                        catch (Exception ignored) {}
+                        player = null;
+                    }
+
+                    if(in != null) {
+                        try { in.close(); }
+                        catch (Exception ignored) {}
+                    }
+                }
+                if (!loop) break;
+            }
+        }, "mp3-play-thread");
+        thread.setDaemon(true);
+        thread.start();
+    }
+
+    public synchronized void stop() {
+        this.running = false;
+    }
+
+    public synchronized void release() {
+        running = false;
+        if(thread != null) {
+            thread.interrupt();
+            // 스레드 실제로 종료되기 전 참조 삭제 방지
+            try { thread.join(500); } 
+            catch (InterruptedException ignored) {}
+            thread = null;
+        }
+
+    }
+}
