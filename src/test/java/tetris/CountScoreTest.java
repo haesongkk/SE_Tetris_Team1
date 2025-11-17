132654109f399a8aea90a13ba55375411e3962d1
jbjihwan <jbgihwan@seoultech.ac.kr>
2025-11-17 23:01:50 +0900

    Action fail 원인 수정

--- src/test/java/tetris/CountScoreTest.java
+++ src/test/java/tetris/CountScoreTest.java
@@ -1,5 +1,7 @@
 package tetris;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
